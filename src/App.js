b0fb02b1ac3ba5f2d28fe93b34f082a940652668
Brian <brian.holland.pr@gmail.com>
2020-05-16 21:04:25 -0700

    added user search display

--- src/App.js
+++ src/App.js
@@ -11,22 +11,22 @@ class App extends Component {
         loading: false,
     };
 
-    async componentDidMount() {
-        //set loading state while we wait for api request
+    searchUsers = async text => {
         this.setState({ loading: true });
-        //wait for api request to come back
+
         const res = await axios.get(
-            `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+            `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
         );
-        //use setState to change state of users and loading status
-        this.setState({ users: res.data, loading: false });
-    }
+        console.log(res.data);
+        this.setState({ users: res.data.items, loading: false });
+    };
+
     render() {
         return (
             <div className="App">
                 <Navbar title="GitHub Finder" icon="fab fa-github" />
                 <div className="container">
-                    <Search />
+                    <Search searchUsers={this.searchUsers} />
                     <Users
                         loading={this.state.loading}
                         users={this.state.users}
